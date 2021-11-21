f19e246ca75b5dd236d779ba7bba1682a5884ae9
ren <ren@vosu.jp>
2021-11-22 03:02:20 +0900

    layout作成とfont導入

---
+++ app/components/layout.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Layout({
+  children
+}:{
+  children: React.ReactNode 
+}) {
+  return (
+    <div className=''>
+      <header className='flex items-center w-full h-28 border-b border-gray-900 border-opacity-20'>
+        <h1 className='pt-2.5 pl-10 font-hind text-5xl font-black'>VOSU</h1>
+      </header>
+      <main>{ children }</main>
+      <footer className='flex justify-center items-center w-full h-20 border-t border-gray-900 border-opacity-20'>
+        <div className='pt-2 text-sm'>©︎2021, VOSU All right reserved.</div>
+      </footer>
+    </div>
+  )
+}
---
+++ app/components/seo.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Seo({
+  pageTitle,
+  pageDescription,
+}: {
+  pageTitle?: String
+  pageDescription: string
+}) {
+  const siteTitle = 'VOSU'
+  // const homeDescription = 'renの個人ブログ。エンジニアや自営業、フリーランンスの人たちに何か役立つ情報を発信できるよう日々の知見や感じたことをストックしていきます。'
+  const title = pageTitle ? `#{siteTitle} | #{pageTitle}` : siteTitle
+  return (
+    <Head>
+      <title>{ title}</title>
+      <meta name='viewport' content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+      <meta name="description" content={pageDescription} />
+      <link rel='icon' href='/favicon.ico' />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Hind:wght@600;700&display=swap" rel="stylesheet" />
+    </Head>
+  )
+}
