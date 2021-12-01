75c41fc8bf6d13dd5d267ceacdc2435196fbb9ac
ren <ren@vosu.jp>
2021-12-02 02:04:30 +0900

    関連記事パーツの作成

--- app/components/category-button.tsx
+++ app/components/category-button.tsx
@@ -7,6 +7,6 @@ export default function CategoryButton({
   isCurrentCategory: boolean
 }) {
   return (
-    <div className={`flex justify-center items-center w-44 h-14 pt-0.5 mr-10 tracking-wide cursor-pointer font-bold text-xl font-inter border border-gray-900 border-opacity-20 rounded-3xl hover:border-opacity-100 hover:shadow-lg ${isCurrentCategory ? 'text-white bg-gray-900 border-transparent' : ''}`}>{ children }</div>
+    <div className={`flex justify-center items-center w-40 h-12 pt-0.5 mr-10 tracking-wide cursor-pointer font-bold text-lg font-inter border border-gray-900 border-opacity-20 rounded-3xl hover:border-opacity-100 hover:shadow-lg ${isCurrentCategory ? 'text-white bg-gray-900 border-transparent' : ''}`}>{ children }</div>
   )
 }
